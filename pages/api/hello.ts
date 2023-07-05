55bd620a4b4cf22412a8f77fbbb44916edfe527e
caioaugustoo <caioamfr@gmail.com>
2023-07-05 12:33:40 -0300

    add cors

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,14 +1,39 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import Cors from "cors";
 import type { NextApiRequest, NextApiResponse } from "next";
+
+const cors = Cors({
+  methods: ["GET"],
+  origin: "https://bot-voting.vercel.app/",
+});
 
 type Data = {
   ip: string;
 };
 
-export default function handler(
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  await runMiddleware(req, res, cors);
+
   let ip = req.headers["x-real-ip"] as string;
 
   const forwardedFor = req.headers["x-forwarded-for"] as string;
